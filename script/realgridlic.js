4338a2f30a210e81bb85f3c45488e648120e3693
김슬기 <yoogi82@gmail.com>
2016-02-15 16:47:26 +0900

    플러스 라이선스

--- script/realgridlic.js
+++ script/realgridlic.js
@@ -1,7 +1,1 @@
-var realGridLic;
-
-if (document.domain == "localhost") {
-	realGridLic = 'upVcPE+wPOnB8WIkZ2gmsypPN9+vaOMYDUkWbkEJahNSClVftxSavsS/Ob2yaOICc6qfV6+Lrnqut+554aV4dwOpEUWgRr5m0F/9996uLI0=';
-} else if (document.domain == "help.realgrid.com") {
-	realGridLic = 'upVcPE+wPOnB8WIkZ2gmsypPN9+vaOMYDUkWbkEJahNSClVftxSavsS/Ob2yaOIC7Rzlv2cBDpZLX6N5c8Gvi1aIR/+OY0FAWJ1Cb0n8X3WqE/o2/QTdZ/75qiwBaTr7';
-}
+var realGridLic = "upVcPE+wPOnB8WIkZ2gmsypPN9+vaOMYDUkWbkEJahNSClVftxSavsS/Ob2yaOICF+cehlIX89WL8pwTXTQK/l7svsLh1nO8ep+Q4ywCYblZaItI0tmvkdBf/fferiyN";
