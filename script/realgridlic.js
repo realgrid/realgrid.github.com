7696dc1962605ac03b3674d5fb5b046aecac5213
onlydel <onlydel@wrw.kr>
2016-03-30 11:44:59 +0900

    라이선스 업데이트

--- script/realgridlic.js
+++ script/realgridlic.js
@@ -1,9 +1,1 @@
-var realGridLic;
-
-if (document.domain == "localhost") {
-	realGridLic = 'upVcPE+wPOnB8WIkZ2gmsypPN9+vaOMYDUkWbkEJahNSClVftxSavsS/Ob2yaOICc6qfV6+Lrnqut+554aV4dwOpEUWgRr5m0F/9996uLI0=';
-} else if (document.domain == "help.realgrid.com") {
-	realGridLic = 'upVcPE+wPOnB8WIkZ2gmsypPN9+vaOMYDUkWbkEJahNSClVftxSavhvD+S0npFWfMdfwd68HU+gxG/WqtkaurVEN9ySJ8a6VVmQO2YNj+YiY3nEiKYzppQ==';
-}
-
- 
+var realGridLic = 'upVcPE+wPOnB8WIkZ2gmsypPN9+vaOMYDUkWbkEJahNSClVftxSavsS/Ob2yaOICF+cehlIX89WL8pwTXTQK/l6odDWZEwhfhQRqdF3gpIY1NqSTTJH1R89iI+PQZ7wH0QeIbtyyIEGTFVqWmNPdyQ==';
