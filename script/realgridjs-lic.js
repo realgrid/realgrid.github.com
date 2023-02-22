faa02e5cbbe14031e9fce4f4fffc2d0d153a5a6c
tacrosst <tacrosst@realgrid.com>
2023-02-22 15:39:36 +0900

    라이선스 키 변경

--- script/realgridjs-lic.js
+++ script/realgridjs-lic.js
@@ -1,2 +1,2 @@
-var realGridJsLic = 'upVcPE+wPOksRHztag2HLt+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgv0x+/TjcSI78+p7IjzZ8+YanyueW1KGsgTDe0mGGVPg1m79yHT0KSfLAv4GxQos+Gkn/JIEkiRbk=';
 var realPivotLic = 'upVcPE+wPOlpAyL7N9uhT0M/nBOseBrflwxYpzGZyYm9cY8amGDkiNBG+1qQWvM8qfK55bUoayAL+B5L0XpktPP7qnhslLPT43ZakWMXSWLTEqzq9j9B4CiorXf9opvENpqrRX3IWpY=';
+var realGridJsLic = 'upVcPE+wPOksRHztag2HLt+UVTCvO3is+83EYTz6U/sTXJR8Yw8Y0WXyjOMqbrgv0x+/TjcSI7/9srbNncEWjw7n7/ElITcuhLGc9fkIdmfRB4hu3LIgQZMVWpaY093J';
